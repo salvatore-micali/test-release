8f9ac1ebcbc54d8c682bed1f9fd2ce7fc040fac0
Salvatore Micali <salvatore.micali@aeaitalia.com>
2021-01-17 11:40:57 +0100

    firstcommit

---
+++ Test_WebApi/Controller/OilPriceController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Test_WebApi.Controller
+{
+    public class OilPriceController : ApiController
+    {
+        [HttpGet]
+        [Route("api/OilPrice/GetOilPriceTrend")] 
+        public string GetOilPriceTrend()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ Test_WebApi/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+using System.Web.SessionState;
+using Test_WebApi.Configuration;
+
+namespace Test_WebApi
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(Test_WebApiConfig.Register);
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
