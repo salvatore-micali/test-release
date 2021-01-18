aefc01b7fc19651e0e53f7fdc7145ee6c957a78b
Salvatore Micali <salvatore.micali@aeaitalia.com>
2021-01-18 19:06:32 +0100

    unit test

---
+++ Test_WebApi.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test_WebApi.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Test_WebApi.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5528d17d-343c-4db0-8e76-02241897396d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Test_WebApi.UnitTests/UnitTest_oilPrice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test_WebApi.BusinessLogic;
+
+namespace Test_WebApi.UnitTests
+{
+    [TestClass]
+    public class UnitTest_oilPrice
+    {
+        [TestMethod]
+        public void OilPriceTrend()
+        {
+            DateTime startDateISO8601 = DateTimeOffset.Parse("2020-01-01T00:00").DateTime; 
+
+            DateTime endDateISO8601 = DateTimeOffset.Parse("2020-01-05T00:00").DateTime;
+        
+            OilPriceManager oilPriceManager = new OilPriceManager();
+
+            var l = oilPriceManager.GetOilPriceTrend(startDateISO8601, endDateISO8601);
+        }
+    }
+}
